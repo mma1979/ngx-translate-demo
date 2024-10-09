6b809137cd991501c365dd4b7496b880e1103181
Mohammed Abdelhay <mamado2000@gmail.com>
2024-10-09 11:13:45 +0300

    ngx-translate-demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslateModule} from '@ngx-translate/core';
+import {SharedModule} from './shared/shared.module';
+import {AsyncPipe} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
+import {LanguageStoreService} from './stores/language-store.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule, SharedModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'untitled';
+constructor(private languageStoreService: LanguageStoreService) {
+  languageStoreService.language$.subscribe((language) => {
+    this.title = language;
+  })
+}
+
+switchLanguage(language: string) {
+  this.languageStoreService.changeLanguage(language);
+}
+
 }
